eeec0230bb445258fc9f4a6807c603e4951b95f1
Chloe Foronda <caforonda103@yahoo.com>
2020-02-11 01:00:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,6 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
-binding.pry
-  
   def print_courses
     self.make_courses
     Course.all.each do |course|
